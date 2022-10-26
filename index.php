7ae7327f66b1d27cddbc448ebe8ecb23564df1be
kasihtaumamang <96900481+kasihtaumamang@users.noreply.github.com>
2022-10-26 11:45:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+	$nm = 5;
+	for($i=0; $i<=$nm; $i++){
+      for($x=$i; $x<=$nm; $x++){
+      	echo $x;
+      }
+      echo "<br />";
+    }
+?>
+</body>
+</html>
